692bf094b8463377c749f8e8a2bb471bb68b44ad
Taavi Aru <taavi.aru@voco.ee>
2025-12-02 09:26:09 +0200

    apache teenus olemasolu kontrollimine ja paigaldamine

---
+++ praks_bash_haldus_2/apache_paigaldamine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# skript mis kontrollib kas Apache teenus on installitud
+# ning kui ei ole siis see teenus paigaldatakse
+
+#kontrollima kas Apache on paigaldatud
+#kui väärtus on 0, siis ei ole paigaldatud
+teenus=$(dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -c 'ok installed')
+echo "staatuse kood on $apache2"
+
+if [ $teenus -eq 0 ]; then #kui ei ole paigaldatud
+  echo "Apache teenus ei ole paigaldatud"
+  echo "Alustame teenuse paigaldamist"
+  apt install apache2 -y
+  echo "Apache teenus on paigaldatud"
+  service apache2 start
+  service apache2 status
+else #on juba paigaldatud
+  echo "Apache teenus on paigaldatud"
+  service apache2 start
+  service apache2 status
+fi
