465c1b49e46ab672697bd73bc23cb8526a7a8e85
Taavi Aru <taavi.aru@voco.ee>
2025-12-02 09:29:59 +0200

    apache teenuse failis kommentaaride lisamine

--- praks_bash_haldus_2/apache_paigaldamine.sh
+++ praks_bash_haldus_2/apache_paigaldamine.sh
@@ -11,12 +11,12 @@ echo "staatuse kood on $apache2"
 if [ $teenus -eq 0 ]; then #kui ei ole paigaldatud
   echo "Apache teenus ei ole paigaldatud"
   echo "Alustame teenuse paigaldamist"
-  apt install apache2 -y
+  apt install apache2 -y #teenuse paigaldamise käsk
   echo "Apache teenus on paigaldatud"
-  service apache2 start
-  service apache2 status
-else #on juba paigaldatud
+  service apache2 start #käivitab teenuse
+  service apache2 status #kuvab teenuse staatuse
+else #kui teenus on juba paigaldatud
   echo "Apache teenus on paigaldatud"
-  service apache2 start
-  service apache2 status
+  service apache2 start #käivitab teenuse
+  service apache2 status #kuvab teenuse staatuse
 fi
